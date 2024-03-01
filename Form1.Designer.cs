cd78120f6a56476e530f898ab2ad0d43d229e55b
MalakAlyazji <161591417+MalakAlyazji@users.noreply.github.com>
2024-03-01 23:56:41 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿
+namespace RPS_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbplayer1 = new System.Windows.Forms.Label();
+            this.lbplayer2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbwhoplayer2 = new System.Windows.Forms.Label();
+            this.lbp1result = new System.Windows.Forms.Label();
+            this.lbp2result = new System.Windows.Forms.Label();
+            this.lbround = new System.Windows.Forms.Label();
+            this.btstop = new System.Windows.Forms.Button();
+            this.btcomputer = new System.Windows.Forms.Button();
+            this.pbp2choice = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pbp1choice = new System.Windows.Forms.PictureBox();
+            this.pbp2photo = new System.Windows.Forms.PictureBox();
+            this.pbwhosturnphoto = new System.Windows.Forms.PictureBox();
+            this.pbp1photo = new System.Windows.Forms.PictureBox();
+            this.btscissors = new System.Windows.Forms.Button();
+            this.btpaper = new System.Windows.Forms.Button();
+            this.btrock = new System.Windows.Forms.Button();
+            this.labeltuen = new System.Windows.Forms.Label();
+            this.lbturn = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbp2choice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbp1choice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbp2photo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbwhosturnphoto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbp1photo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbplayer1
+            // 
+            this.lbplayer1.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbplayer1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lbplayer1.Location = new System.Drawing.Point(224, 213);
+            this.lbplayer1.Name = "lbplayer1";
+            this.lbplayer1.Size = new System.Drawing.Size(125, 46);
+            this.lbplayer1.TabIndex = 9;
+            this.lbplayer1.Text = "Player1";
+            // 
+            // lbplayer2
+            // 
+            this.lbplayer2.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbplayer2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lbplayer2.Location = new System.Drawing.Point(851, 213);
+            this.lbplayer2.Name = "lbplayer2";
+            this.lbplayer2.Size = new System.Drawing.Size(218, 46);
+            this.lbplayer2.TabIndex = 10;
+            this.lbplayer2.Text = "Player2";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 46);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Round:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(12, 548);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(229, 46);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Player1 Result:";
+            // 
+            // lbwhoplayer2
+            // 
+            this.lbwhoplayer2.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbwhoplayer2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lbwhoplayer2.Location = new System.Drawing.Point(12, 605);
+            this.lbwhoplayer2.Name = "lbwhoplayer2";
+            this.lbwhoplayer2.Size = new System.Drawing.Size(229, 46);
+            this.lbwhoplayer2.TabIndex = 16;
+            this.lbwhoplayer2.Text = "Player2 Result:";
+            // 
+            // lbp1result
+            // 
+            this.lbp1result.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbp1result.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lbp1result.Location = new System.Drawing.Point(233, 548);
+            this.lbp1result.Name = "lbp1result";
+            this.lbp1result.Size = new System.Drawing.Size(90, 36);
+            this.lbp1result.TabIndex = 17;
+            this.lbp1result.Tag = "0";
+            this.lbp1result.Text = "0";
+            // 
+            // lbp2result
+            // 
+            this.lbp2result.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbp2result.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lbp2result.Location = new System.Drawing.Point(233, 605);
+            this.lbp2result.Name = "lbp2result";
+            this.lbp2result.Size = new System.Drawing.Size(90, 36);
+            this.lbp2result.TabIndex = 18;
+            this.lbp2result.Tag = "0";
+            this.lbp2result.Text = "0";
+            // 
+            // lbround
+            // 
+            this.lbround.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbround.Location = new System.Drawing.Point(126, 9);
+            this.lbround.Name = "lbround";
+            this.lbround.Size = new System.Drawing.Size(40, 36);
+            this.lbround.TabIndex = 19;
+            this.lbround.Text = "0";
+            // 
+            // btstop
+            // 
+            this.btstop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btstop.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btstop.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btstop.Location = new System.Drawing.Point(483, 595);
+            this.btstop.Name = "btstop";
+            this.btstop.Size = new System.Drawing.Size(169, 46);
+            this.btstop.TabIndex = 20;
+            this.btstop.Text = "Stop";
+            this.btstop.UseVisualStyleBackColor = false;
+            this.btstop.Click += new System.EventHandler(this.btstop_Click);
+            // 
+            // btcomputer
+            // 
+            this.btcomputer.BackgroundImage = global::RPS_project.Properties.Resources.computer;
+            this.btcomputer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btcomputer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btcomputer.Location = new System.Drawing.Point(939, 548);
+            this.btcomputer.Name = "btcomputer";
+            this.btcomputer.Size = new System.Drawing.Size(110, 105);
+            this.btcomputer.TabIndex = 21;
+            this.btcomputer.UseVisualStyleBackColor = true;
+            this.btcomputer.Click += new System.EventHandler(this.btcomputer_Click);
+            // 
+            // pbp2choice
+            // 
+            this.pbp2choice.Image = global::RPS_project.Properties.Resources.Q2;
+            this.pbp2choice.Location = new System.Drawing.Point(845, 273);
+            this.pbp2choice.Name = "pbp2choice";
+            this.pbp2choice.Size = new System.Drawing.Size(143, 74);
+            this.pbp2choice.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbp2choice.TabIndex = 8;
+            this.pbp2choice.TabStop = false;
+            this.pbp2choice.Tag = "?";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::RPS_project.Properties.Resources.Middlephoto;
+            this.pictureBox5.InitialImage = global::RPS_project.Properties.Resources.Middlephoto;
+            this.pictureBox5.Location = new System.Drawing.Point(450, 213);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(261, 170);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pbp1choice
+            // 
+            this.pbp1choice.Image = global::RPS_project.Properties.Resources.Q1;
+            this.pbp1choice.Location = new System.Drawing.Point(215, 273);
+            this.pbp1choice.Name = "pbp1choice";
+            this.pbp1choice.Size = new System.Drawing.Size(143, 74);
+            this.pbp1choice.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbp1choice.TabIndex = 6;
+            this.pbp1choice.TabStop = false;
+            this.pbp1choice.Tag = "?";
+            // 
+            // pbp2photo
+            // 
+            this.pbp2photo.Image = global::RPS_project.Properties.Resources.normal;
+            this.pbp2photo.Location = new System.Drawing.Point(845, 94);
+            this.pbp2photo.Name = "pbp2photo";
+            this.pbp2photo.Size = new System.Drawing.Size(143, 74);
+            this.pbp2photo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbp2photo.TabIndex = 5;
+            this.pbp2photo.TabStop = false;
+            this.pbp2photo.Tag = "normal\'";
+            // 
+            // pbwhosturnphoto
+            // 
+            this.pbwhosturnphoto.Image = global::RPS_project.Properties.Resources.p1turn;
+            this.pbwhosturnphoto.Location = new System.Drawing.Point(516, 81);
+            this.pbwhosturnphoto.Name = "pbwhosturnphoto";
+            this.pbwhosturnphoto.Size = new System.Drawing.Size(143, 74);
+            this.pbwhosturnphoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbwhosturnphoto.TabIndex = 4;
+            this.pbwhosturnphoto.TabStop = false;
+            this.pbwhosturnphoto.Tag = "p1";
+            // 
+            // pbp1photo
+            // 
+            this.pbp1photo.Image = global::RPS_project.Properties.Resources.normal;
+            this.pbp1photo.Location = new System.Drawing.Point(215, 81);
+            this.pbp1photo.Name = "pbp1photo";
+            this.pbp1photo.Size = new System.Drawing.Size(143, 74);
+            this.pbp1photo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbp1photo.TabIndex = 3;
+            this.pbp1photo.TabStop = false;
+            this.pbp1photo.Tag = "normal";
+            // 
+            // btscissors
+            // 
+            this.btscissors.BackgroundImage = global::RPS_project.Properties.Resources.scissors;
+            this.btscissors.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btscissors.Location = new System.Drawing.Point(811, 404);
+            this.btscissors.Name = "btscissors";
+            this.btscissors.Size = new System.Drawing.Size(165, 115);
+            this.btscissors.TabIndex = 2;
+            this.btscissors.Tag = "s";
+            this.btscissors.UseVisualStyleBackColor = true;
+            this.btscissors.Click += new System.EventHandler(this.btclick);
+            // 
+            // btpaper
+            // 
+            this.btpaper.BackgroundImage = global::RPS_project.Properties.Resources.paper;
+            this.btpaper.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btpaper.Location = new System.Drawing.Point(516, 404);
+            this.btpaper.Name = "btpaper";
+            this.btpaper.Size = new System.Drawing.Size(165, 115);
+            this.btpaper.TabIndex = 1;
+            this.btpaper.Tag = "p";
+            this.btpaper.UseVisualStyleBackColor = true;
+            this.btpaper.Click += new System.EventHandler(this.btclick);
+            // 
+            // btrock
+            // 
+            this.btrock.BackgroundImage = global::RPS_project.Properties.Resources.rock;
+            this.btrock.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btrock.Location = new System.Drawing.Point(215, 404);
+            this.btrock.Name = "btrock";
+            this.btrock.Size = new System.Drawing.Size(165, 115);
+            this.btrock.TabIndex = 0;
+            this.btrock.Tag = "r";
+            this.btrock.UseVisualStyleBackColor = true;
+            this.btrock.Click += new System.EventHandler(this.btclick);
+            // 
+            // labeltuen
+            // 
+            this.labeltuen.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labeltuen.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.labeltuen.Location = new System.Drawing.Point(477, 32);
+            this.labeltuen.Name = "labeltuen";
+            this.labeltuen.Size = new System.Drawing.Size(91, 46);
+            this.labeltuen.TabIndex = 23;
+            this.labeltuen.Text = "Turn:";
+            // 
+            // lbturn
+            // 
+            this.lbturn.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbturn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lbturn.Location = new System.Drawing.Point(563, 32);
+            this.lbturn.Name = "lbturn";
+            this.lbturn.Size = new System.Drawing.Size(128, 46);
+            this.lbturn.TabIndex = 24;
+            this.lbturn.Text = "Player1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1074, 660);
+            this.Controls.Add(this.lbturn);
+            this.Controls.Add(this.labeltuen);
+            this.Controls.Add(this.btcomputer);
+            this.Controls.Add(this.btstop);
+            this.Controls.Add(this.lbround);
+            this.Controls.Add(this.lbp2result);
+            this.Controls.Add(this.lbp1result);
+            this.Controls.Add(this.lbwhoplayer2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbplayer2);
+            this.Controls.Add(this.lbplayer1);
+            this.Controls.Add(this.pbp2choice);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pbp1choice);
+            this.Controls.Add(this.pbp2photo);
+            this.Controls.Add(this.pbwhosturnphoto);
+            this.Controls.Add(this.pbp1photo);
+            this.Controls.Add(this.btscissors);
+            this.Controls.Add(this.btpaper);
+            this.Controls.Add(this.btrock);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbp2choice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbp1choice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbp2photo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbwhosturnphoto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbp1photo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btrock;
+        private System.Windows.Forms.Button btpaper;
+        private System.Windows.Forms.Button btscissors;
+        private System.Windows.Forms.PictureBox pbp1photo;
+        private System.Windows.Forms.PictureBox pbwhosturnphoto;
+        private System.Windows.Forms.PictureBox pbp2photo;
+        private System.Windows.Forms.PictureBox pbp1choice;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pbp2choice;
+        private System.Windows.Forms.Label lbplayer1;
+        private System.Windows.Forms.Label lbplayer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbwhoplayer2;
+        private System.Windows.Forms.Label lbp1result;
+        private System.Windows.Forms.Label lbp2result;
+        private System.Windows.Forms.Label lbround;
+        private System.Windows.Forms.Button btstop;
+        private System.Windows.Forms.Button btcomputer;
+        private System.Windows.Forms.Label labeltuen;
+        private System.Windows.Forms.Label lbturn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPS_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       enum enplayer { player1,player2};
+       enum enplayer2 { person,computer};
+       enum enwinner { player1, player2 ,Draw};
+        enplayer playerturn = enplayer.player1;
+        enplayer2 player2turn = enplayer2.person;
+        
+        struct stgameresult
+        {
+          public  enwinner winner;
+            public short player1win;
+            public short player2win;
+            public short nrounds;
+        }
+        stgameresult gameresult;
+        void roundresult(PictureBox p1, PictureBox p2)
+        {
+            {
+                if (p1.Tag.ToString() == p2.Tag.ToString())
+                {
+                    gameresult.winner = enwinner.Draw;
+                    return;
+                }
+                if (p1.Tag.ToString() == "r"&& p2.Tag.ToString() == "p")
+                {
+                     gameresult.winner = enwinner.player2;
+                    
+                }
+                else if (p1.Tag.ToString() == "p" && p2.Tag.ToString() == "s")
+                {
+                    gameresult.winner = enwinner.player2;
+                   
+                }
+                else if (p1.Tag.ToString() == "s"&& p2.Tag.ToString() == "r")
+                {
+                    gameresult.winner = enwinner.player2;
+                }
+                else { 
+                    gameresult.winner = enwinner.player1;
+                }
+            }
+        }
+        void picturechoice(PictureBox pb)
+        {
+            if (pb.Tag.ToString() == "r")
+                pb.Image = Image.FromFile(@"C:\Users\hp\Downloads\Telegram Desktop\rock.jpg");
+            else if (pb.Tag.ToString() == "p")
+            {
+                pb.Image = Image.FromFile(@"C:\Users\hp\Downloads\Telegram Desktop\paper.jpg");
+            }
+            else{
+                pb.Image = Image.FromFile(@"C:\Users\hp\Downloads\Telegram Desktop\scissors.jpg");
+            }
+        }
+        void result()
+        {
+            switch (gameresult.winner)
+            {
+                case enwinner.player1:
+                    pbp1photo.Image = Image.FromFile(@"C:\Users\hp\Downloads\Telegram Desktop\win.jpg");
+                    pbp2photo.Image = Image.FromFile(@"C:\Users\hp\Downloads\Telegram Desktop\fail.jpg");
+                    gameresult.player1win++;
+                    gameresult.nrounds++;
+                    break;
+                case enwinner.player2:
+                    pbp1photo.Image = Image.FromFile(@"C:\Users\hp\Downloads\Telegram Desktop\fail.jpg");
+                    pbp2photo.Image = Image.FromFile(@"C:\Users\hp\Downloads\Telegram Desktop\win.jpg");
+                    gameresult.player2win++;
+                    gameresult.nrounds++;
+                    break;
+                default:
+                    gameresult.nrounds++;
+                    pbp1photo.Image = Image.FromFile(@"C:\Users\hp\Downloads\Telegram Desktop\normal.jpg");
+                    pbp2photo.Image = Image.FromFile(@"C:\Users\hp\Downloads\Telegram Desktop\normal.jpg");
+                    break;
+            }
+            lbp1result.Text = gameresult.player1win.ToString();
+            lbp2result.Text = gameresult.player2win.ToString();
+            lbround.Text = gameresult.nrounds.ToString();
+        }
+        void changepicture(Button bt)
+        {
+             switch (playerturn)
+             {
+                case enplayer.player1:
+                    pbp1choice.Tag = bt.Tag;
+                    picturechoice(pbp1choice);
+                    if (player2turn == enplayer2.person)
+                    {
+                        playerturn = enplayer.player2;
+                        lbturn.Text = "Player2";
+                        btcomputer.Visible = false;
+                    }
+
+                    pbwhosturnphoto.Image = Image.FromFile(@"C:\Users\hp\Downloads\Telegram Desktop\p2turn.jpg");
+                    break;
+                case enplayer.player2:
+                    pbp2choice.Tag = bt.Tag;
+                    picturechoice(pbp2choice);
+                    playerturn = enplayer.player1;
+                    lbturn.Text = "Player1";
+                    pbwhosturnphoto.Image = Image.FromFile(@"C:\Users\hp\Downloads\Telegram Desktop\p1turn.jpg");
+                    roundresult(pbp1choice, pbp2choice);
+                     result();
+                        break;
+
+                }
+            if (player2turn == enplayer2.computer)
+            {
+                computerchoice();
+                picturechoice(pbp2choice);
+                playerturn = enplayer.player1;
+                lbturn.Text = "Player1";
+                pbp1photo.Image = Image.FromFile(@"C:\Users\hp\Downloads\Telegram Desktop\p1turn.jpg");
+                roundresult(pbp1choice, pbp2choice);
+                result();
+            }
+        }
+        void computerchoice()
+        {
+            Random random = new Random();
+            byte rand = (byte)random.Next(1, 3);
+            switch (rand)
+            {
+                case 1:
+                    pbp2choice.Tag = "r";
+                    break;
+                case 2:
+                    pbp2choice.Tag = "p";
+                    break;
+                case 3:
+                    pbp2choice.Tag = "s";
+                    break;
+            }
+        }
+        private void btclick(object sender, EventArgs e)
+        {
+            changepicture((Button )sender);
+        }
+        private void btcomputer_Click(object sender, EventArgs e)
+        {
+            lbwhoplayer2.Text = "Computer: ";
+            lbplayer2.Text = lbwhoplayer2.Text;
+            player2turn = enplayer2.computer;
+        }
+
+        private void btstop_Click(object sender, EventArgs e)
+        {
+            if(gameresult.player1win> gameresult.player2win)
+            {
+                MessageBox.Show("Player 1 is the winner :)", "Final Result",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }else if (gameresult.player1win < gameresult.player2win)
+            {
+                if (player2turn == enplayer2.computer)
+                {
+                    MessageBox.Show("Computer  is the winner :)", "Final Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                MessageBox.Show("Player 2 is the winner :)", "Final Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Draw :)", "Final Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            this.Close();
+        }
+    }
+}
